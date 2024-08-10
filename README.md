782b70efd99f38edb1b0f21b11be0b05164db35c
Heyan Zhu <zhu_heyan@163.com>
2024-08-10 19:40:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Brainfuck Interpreter (JavaScript)
+
+A toy Brainfuck interpreter implemented in JavaScript.
+
+## Overview
+
+This is a toy project that brings a JavaScript implementation of the Brainfuck interpreter without any dependencies besides Node.js.
+
+## Usage
+
+To run the interpreter, use the following command:
+
+```
+node bf.cjs [-h] [-v] [<file.bf> or -c <code>]
+```
+
+### Options
+
+- `-h`, `--help`: Print help message and exit.
+- `-v`: Enable verbose mode.
+- `-m <size>`: Set the memory size (default: 128).
+- `<file.bf>`: Run the Brainfuck code from a file.
+- `-c <code>`: Run the Brainfuck code from a string.
+
+### Example Use Cases
+
+- Run a Brainfuck program from a file: 
+
+    ```
+    node bf.cjs hello-world.bf
+    ```
+
+- Run a Brainfuck program from a string: 
+
+    ```
+    node bf.cjs -c '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'
+    ```
+
+## Project Structure
+
+* `bf.cjs`: The Brainfuck interpreter implementation.
+* `hello-world.bf`: An example Brainfuck program, obtained from [wikipedia](https://en.wikipedia.org/wiki/Brainfuck).
+* `README.md`: This file.
+
+---
+
+Last update: Aug. 10, 2024
---
+++ bf.cjs
@@ -0,0 +1,140 @@
+"use strict";
+
+const fs = require("fs").promises;
+
+let MEM_SIZE = 128;
+let verbose = false;
+
+const description = "A toy BrainFuck interpreter implemented in JavaScript.";
+const usage = `
+Usage: node ${process.argv[1].replace("\\", "/").split("/").pop()} [-h] [-v] [<file.bf> or -c <code>]
+
+Options:
+  -h, --help  Print this help message (ignore other options)
+  -v          Enable verbose mode
+  -m <size>   Set memory size (default: ${MEM_SIZE})
+`.trim();
+
+const args = process.argv.slice(2);
+
+async function parseArgs() {
+	let codeInput = "";
+
+	if (args.length === 0) {
+		console.log("Invalid number of arguments");
+		console.log(usage);
+		process.exit(0);
+	}
+
+	for (let i = 0; i < args.length; i++) {
+		if (args[i][0] === "-") { // options
+			if (args[i][1] === "m" && /^\d+$/.test(args[i + 1])) {
+				MEM_SIZE = parseInt(args[i + 1]);
+				i += 1;
+			} else if ((args[i][1] === "h" || args[i] === "--help") && i === 0) {
+				console.log(description);
+				console.log(usage);
+				process.exit(0);
+			} else if (args[i][1] === "v") {
+				verbose = true;
+			} else if (args[i][1] === "c" && args[i + 1] !== undefined) {
+				if (codeInput !== "") {
+					console.log("Code already specified by file name");
+					console.log(usage);
+					process.exit(0);
+				}
+				codeInput = args[i + 1];
+				i += 1;
+			} else {
+				console.log("Invalid option: " + args[i]);
+				console.log(usage);
+				process.exit(0);
+			}
+		} else { // run file
+			if (codeInput !== "") {
+				console.log("Code already specified by -c option");
+				console.log(usage);
+				process.exit(0);
+			}
+			await fs.readFile(args[i], "utf8")
+				.then(code => codeInput = code)
+				.catch(() => {
+					console.log(`Failed to read file: "${args[i]}"`);
+					process.exit(0);
+				});
+		}
+	}
+
+	if(verbose){
+		console.log("Memory size: " + MEM_SIZE);
+		console.log("Input bf code: \n\n" + codeInput + "\n");
+	}
+	return codeInput;
+}
+
+function bf(code) {
+	let ptr = 0;
+	let mem = new Array(MEM_SIZE).fill(0);
+	for (let i = 0; i < code.length; i++) {
+		const t = code[i];
+		switch (t) {
+			case ">": {
+				ptr += 1;
+				if (ptr >= MEM_SIZE) {
+					console.log("[Error] Memory overflow at character " + i);
+					process.exit(0);
+				}
+				break;
+			}
+			case "<": {
+				ptr -= 1;
+				if (ptr < 0) {
+					console.log("[Error] Memory underflow at character " + i);
+					process.exit(0);
+				}
+				break;
+			}
+			case "+": {
+				mem[ptr] += 1;
+				break;
+			}
+			case "-": {
+				mem[ptr] -= 1;
+				break;
+			}
+			case ".": {
+				process.stdout.write(String.fromCharCode(mem[ptr]));
+				break;
+			}
+			case ",": {
+				mem[ptr] = process.stdin.read();
+				break;
+			}
+			case "[": {
+				if (mem[ptr] == 0) {
+					let level = 1;
+					while (level > 0) {
+						i += 1;
+						if (code[i] == "[") level++;
+						if (code[i] == "]") level--;
+					}
+				}
+				break;
+			}
+			case "]": {
+				if (mem[ptr] != 0) {
+					let level = 1;
+					while (level > 0) {
+						i -= 1;
+						if (code[i] == "]") level++;
+						if (code[i] == "[") level--;
+					}
+				}
+				break;
+			}
+		}
+	}
+}
+
+parseArgs().then(code => bf(code));
+
